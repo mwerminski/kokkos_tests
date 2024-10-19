340cdc41375c02543eb4d6660e0782c8aa68a682
szylkret <contact@szylkret.dev>
2024-10-19 21:54:47 +0200

    Update gitignore; add OpenMP and example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <Kokkos_Core.hpp>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+struct hello_world {
+
+  KOKKOS_INLINE_FUNCTION
+  void operator()(const int i) const {
+    Kokkos::printf("Hello from i = %i\n", i);
+  }
+};
+
+int main(int argc, char* argv[]) {
+  Kokkos::initialize(argc, argv);
+  printf("Hello World on Kokkos execution space %s\n",
+         Kokkos::DefaultExecutionSpace::name());
+
+  Kokkos::parallel_for("HelloWorld", 15, hello_world());
+  Kokkos::finalize();
+  return 0;
 }
