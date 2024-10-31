461ceed528b49b5afbe79b0bf350182306d3265d
Mariusz Werminski <mwerminski@wp.pl>
2024-10-31 19:39:43 +0000

    Add image loading to view

--- example_apps/convolution_test/conv_test.cpp
+++ example_apps/convolution_test/conv_test.cpp
@@ -6,13 +6,35 @@
 #include <Kokkos_DualView.hpp>
 #include <bitmap_image.hpp>
 
-#define RANK 3
+#define CHANNELS 3
+#define RED_CHANNEL 0
+#define BLUE_CHANNEL 1
+#define GREEN_CHANNEL 2
 
-using DualView = Kokkos::DualView<double***, Kokkos::LayoutLeft, Kokkos::Cuda>;
+using DualView = Kokkos::DualView<int***, Kokkos::LayoutLeft, Kokkos::Cuda>;
 
 static DualView read_bitmap(std::string path) {
       bitmap_image image(path);
-      return DualView("img data");
+      const std::size_t height = image.height();
+      const std::size_t width = image.width();
+
+      DualView image_dual_view("img_data", width, height, CHANNELS);
+
+      auto image_view = image_dual_view.view_host();
+      for (std::size_t y = 0; y < height; ++y) {
+          for (std::size_t x = 0; x < width; ++x) {
+               rgb_t color;
+               image.get_pixel(x, y, color);
+               image_view(x,y,RED_CHANNEL) = color.red;
+               image_view(x,y,GREEN_CHANNEL) = color.green;
+               image_view(x,y,BLUE_CHANNEL) = color.blue;
+          }
+      }
+
+      image_dual_view.modify_host();
+      image_dual_view.sync_device();
+
+      return image_dual_view;
   }
 
 int main(int argc, char* argv[]) {
